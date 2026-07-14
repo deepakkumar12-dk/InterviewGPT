0af229db1aa72265abdaed48879bb08e5492b583
deepakkumar12-dk <kumawatdipak033@gmail.com>
2026-07-14 18:35:32 +0530

    Fix resume upload content type

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://interviewgpt-backend-bunj.onrender.com",
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 export default api;
