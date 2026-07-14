8b6e1884b89d728b7cc1925ce965c656a9e69e53
deepakkumar12-dk <kumawatdipak033@gmail.com>
2026-07-14 18:14:33 +0530

    Add vercel routing

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ app = FastAPI(title="InterviewGPT API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173",
+    allow_origins=[
         "https://interview-gpt-seven.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
