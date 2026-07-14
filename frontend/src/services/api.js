b5939e8f462d6c3ece3619bf82160ec4779027c9
deepakkumar12-dk <kumawatdipak033@gmail.com>
2026-07-14 17:35:03 +0530

    Enable CORS for deployment

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "https://interviewgpt-backend-bunj.onrender.com",
   headers: {
-    "Content-Type": "multipart/form-data",
+    "Content-Type": "application/json",
   },
 });
 
